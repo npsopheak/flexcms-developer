c82826c4e2b71f316ce6fd8b62a277e47aa198e3
npsopheak <npsopheak@gmail.com>
2017-05-12 17:04:04 +0700

    update config

--- src/config/flexcms.php
+++ src/config/flexcms.php
@@ -16,8 +16,32 @@ return [
 
 	],
 
+	'system' => [
+
+		// Mostly is the app client id or api client id in the nodejs service
+		'request_id' => 'X-XX-Request-ID',
+
+		// The session id 
+		'session_id' => 'X-XX-Connect-ID',
+
+		// The key encryption name
+		'encrypt_key_id' => 'X-XX-Sign-Key',
+
+		// User Agent name
+		'user_agent' => 'Flex Dashboard'
+	],
+
     'api' => [
-    	'endpoint' => 'http://localhost:8000/api/v1/'
+    	'endpoint' => 'http://localhost:8000/api/v1/',
+
+		'ws_endpoint' => 'http://localhost:8000/api/v1/',
+
+		'use_x_forward_for' => false,
+
+		'default_content_type' => 'application/json',
+
+		// Value for request id
+		'request_id' => 'MGUwMTIwZDEyNmYzZTA4ZDI5ZGFkYzcxZWFmMjhhOGU1MDU3OWNjNzRmZDA1ZWUzZjkyZmU5NTc0OWI1ZjE4Nw=='
     ]
 
 ];
