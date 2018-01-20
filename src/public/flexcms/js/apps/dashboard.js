d12b65cf9031a2dc0630621772d4722725df9752
npsopheak <npsopheak@gmail.com>
2018-01-20 14:59:26 +0700

    update new bower component

--- src/public/flexcms/js/apps/dashboard.js
+++ src/public/flexcms/js/apps/dashboard.js
@@ -1,3 +1,4 @@
 app = angular.module('StarterApp', ['ngMaterial','as.sortable',  'uiGmapgoogle-maps', 'ngRoute', 'ngSanitize', 
-	'dndLists', 'ngResource', 'ngFileUpload', 'ngMap' , 'cl.paging', 'ngMaterialDatePicker','selectize', 'bw.paging']);
+	'dndLists', 'ngResource', 'ngFileUpload', 'ngMap' , 'cl.paging', 'ngMaterialDatePicker','selectize', 'bw.paging', 
+	'720kb.datepicker']);
 namespace.app = app;
